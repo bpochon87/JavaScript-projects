35b18b6f5cb1a0b139f6eaf0b31b63b249d95808
bpochon87 <83652607+bpochon87@users.noreply.github.com>
2022-02-07 12:16:31 -0700

    First commit

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,13 @@
+var intro=("Good day, friend."); // Assigning variable 'intro' its string value.
+    window.alert(intro); // Pop-up window displaying 'intro' variable.
+
+var family="The Pochons", mom="Dale Dee", dad="Ron", wife="Sara", husband="Brady", catSon="Suerte" // Assigning multiple variables
+var salutation="It is a pleasure meeting you."; // Assing variable 'salutation' its string value.
+
+document.write(intro + salutation); // Concatenated variables 'intro' and 'salutation'.
+
+document.write(catSon);   // Displaying value of catSon.
+
+document.write((3 + 9) / 4); // Displaying value of expression.
+
+document.write(intro.fontcolor("blue")) // Assigned a deprecated method to color 'intro' blue.
