eb836ddb164dbf39dac6802256034d1bc657b597
bpochon87 <83652607+bpochon87@users.noreply.github.com>
2022-02-10 09:22:22 -0700

    Step 140 commit.

---
+++ Basic_JavaScript_Projects/Project6_ternary_operators_constructors/JS/main.js
@@ -0,0 +1,57 @@
+// Creating an object constructor
+function Person(name, hairColor, eyeColor, height, weight)
+{
+    this.name = name;
+    this.hairColor = hairColor;
+    this.eyeColor = eyeColor;
+    this.height = height;
+    this.weight = weight;
+}
+
+var person01 = new Person('Brady', 'Brown', 'Brown', '5\'10', 170);
+var person02 = new Person('Sara', 'Brown', 'Brown', '5\'4', 130);
+function myFunc()
+{
+    document.getElementById("newPeople").innerHTML = "Let's welcome " + person01.name + " and " + person02.name + " to the club!";
+}
+
+// Tech Academy's example for a nested function
+function countFunc()
+{
+    document.getElementById("counting").innerHTML = count();
+    function count()
+    {
+        var start = 9;
+        function plusOne()
+        {
+            start += 1;
+        }
+        plusOne();
+        return start;
+    }
+}
+
+// Brady nested function example. I need a second pair of eyes. Not working.
+function greetFunc()
+{
+    document.getElementById("greeting").innerHTML = introFunc();
+    introFunc()
+    {
+        var hi = "hello";
+        var bye = "goodbye";
+        function outroFunc()
+        {
+            var full = "We said" + hi + "and" + bye;
+        }
+        outroFunc();
+        return full;
+    }
+}
+
+// Ternary example
+function voteFunc()
+{
+    var userAge = document.getElementById("age").value;
+    var canVote = (userAge >= 18) ? "You're able to vote":"You're unable to vote";
+    document.getElementById("voteAble").innerHTML = canVote + " in US elections.";    
+}
