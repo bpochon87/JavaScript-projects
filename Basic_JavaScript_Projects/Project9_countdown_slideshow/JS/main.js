19a5048fef9b1ae0b00b2a389c8f4521487c95e0
bpochon87 <83652607+bpochon87@users.noreply.github.com>
2022-02-12 07:09:06 -0700

    Review

---
+++ Basic_JavaScript_Projects/Project9_countdown_slideshow/JS/main.js
@@ -0,0 +1,19 @@
+// Countdown function for timer
+function countdown()
+{
+    var seconds = document.getElementById("seconds").value;
+    
+    function tick()
+    {
+        seconds = seconds - 1;
+        timer.innerHTML = seconds;
+        var time = setTimeout(tick, 1000);
+        if (seconds == -1)
+        {
+            alert("Time is Up.");
+            clearTimeout(time);
+            timer.innerHTML = "";
+        }
+    }
+    tick();
+}
