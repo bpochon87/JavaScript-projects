6ecba639217001ac41d80cc0f91b4fa42b5b6f81
bpochon87 <83652607+bpochon87@users.noreply.github.com>
2022-02-14 12:11:54 -0700

    Project 9

--- Basic_JavaScript_Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic_JavaScript_Projects/Project9_countdown_slideshow/JS/main.js
@@ -1,19 +1,40 @@
-// Countdown function for timer
-function countdown()
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n)
 {
-    var seconds = document.getElementById("seconds").value;
-    
-    function tick()
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n)
+{
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n)
+{
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length)
     {
-        seconds = seconds - 1;
-        timer.innerHTML = seconds;
-        var time = setTimeout(tick, 1000);
-        if (seconds == -1)
-        {
-            alert("Time is Up.");
-            clearTimeout(time);
-            timer.innerHTML = "";
-        }
+        slideIndex = 1;
     }
-    tick();
+    if (n < 1)
+    {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++)
+    {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++)
+    {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    
 }
